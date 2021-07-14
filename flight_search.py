25a0d736bbd686fbabee68df03ee4f026c552df9
Uzzal Hossain <de3dm3nu11@gmail.com>
2021-07-14 16:09:18 +0600

    All done without notification sms

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,7 @@
 import requests
 
+from flight_data import FlightData
+
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_API_KEY = "bRFIy_maJip1WPnLV1h5VCG5xCHldNIj"
 
@@ -16,6 +18,43 @@ class FlightSearch:
                                  headers=search_headers)
         code = response.json()["locations"][0]["code"]
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        search_endpoint = f"{TEQUILA_ENDPOINT}/v2/search"
+        search_headers = {"apikey": TEQUILA_API_KEY}
+        search_params = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP",
+        }
+
+        response = requests.get(url=search_endpoint, params=search_params, headers=search_headers)
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
 
 
 
--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 # Day 39 starting
 from data_manager import DataManager
+from flight_search import FlightSearch
 from flight_data import FlightData
-flight_data = FlightData()
+import datetime
 
+#flight_data = FlightData()
+flight_search = FlightSearch()
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
+ORIGIN_CITY_IATA = "LON"
 
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
 
-else:
-    for row in sheet_data:
-        city_name = row["city"]
-        city_code = row["iataCode"]
-        price = flight_data.get_flight_data(city_code)
-        price = price["data"][0]["price"]
-        print(f"{city_name}: {price}")
+tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1))
+six_months = (datetime.datetime.now() + datetime.timedelta(days=(6 * 30)))
 
-    # print(f"{sheet_data}\n\n")
-    # print(f"{sheet_data[0]['city']}\n\n")
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months,
+    )
+
+    if destination["lowestPrice"] < flight.price:
+        print("Price is less than old price")
+    else:
+        print("price is more than old price")
+
+
+
